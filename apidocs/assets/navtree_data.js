c3ab67172604f9d91b1051849d53de5c9e260243
simonetripodi <simone.tripodi@gmail.com>
2012-03-20 09:08:06 +0100

    Wagon: Deploying site to repository

--- apidocs/assets/navtree_data.js
+++ apidocs/assets/navtree_data.js
@@ -1,5 +1,5 @@
 var NAVTREE_DATA =
-[ [ "org.nnsoft.guice.sli4j.acl", "org/nnsoft/guice/sli4j/acl/package-summary.html", [ [ "Description", "org/nnsoft/guice/sli4j/acl/package-descr.html", null, "" ], [ "Classes", null, [ [ "ACLLoggerInjector", "org/nnsoft/guice/sli4j/acl/ACLLoggerInjector.html", null, "" ], [ "ACLLoggingModule", "org/nnsoft/guice/sli4j/acl/ACLLoggingModule.html", null, "" ] ]
+[ [ "org.nnsoft.guice.sli4j.log4j", "org/nnsoft/guice/sli4j/log4j/package-summary.html", [ [ "Description", "org/nnsoft/guice/sli4j/log4j/package-descr.html", null, "" ], [ "Classes", null, [ [ "Log4JLoggerInjector", "org/nnsoft/guice/sli4j/log4j/Log4JLoggerInjector.html", null, "" ], [ "Log4jLoggingModule", "org/nnsoft/guice/sli4j/log4j/Log4jLoggingModule.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -1,6 +1,6 @@
 var DATA = [
-      { id:0, label:"org.nnsoft.guice.sli4j.acl", link:"org/nnsoft/guice/sli4j/acl/package-summary.html", type:"package" },
-      { id:1, label:"org.nnsoft.guice.sli4j.acl.ACLLoggerInjector", link:"org/nnsoft/guice/sli4j/acl/ACLLoggerInjector.html", type:"class" },
-      { id:2, label:"org.nnsoft.guice.sli4j.acl.ACLLoggingModule", link:"org/nnsoft/guice/sli4j/acl/ACLLoggingModule.html", type:"class" }
+      { id:0, label:"org.nnsoft.guice.sli4j.log4j", link:"org/nnsoft/guice/sli4j/log4j/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.guice.sli4j.log4j.Log4JLoggerInjector", link:"org/nnsoft/guice/sli4j/log4j/Log4JLoggerInjector.html", type:"class" },
+      { id:2, label:"org.nnsoft.guice.sli4j.log4j.Log4jLoggingModule", link:"org/nnsoft/guice/sli4j/log4j/Log4jLoggingModule.html", type:"class" }
 
     ];
