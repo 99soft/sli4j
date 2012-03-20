782cb6d6cc214edbe7661fc155db194342fbaa60
simonetripodi <simone.tripodi@gmail.com>
2012-03-20 09:08:46 +0100

    Wagon: Deploying site to repository

--- apidocs/assets/navtree_data.js
+++ apidocs/assets/navtree_data.js
@@ -1,5 +1,5 @@
 var NAVTREE_DATA =
-[ [ "org.nnsoft.guice.sli4j.log4j", "org/nnsoft/guice/sli4j/log4j/package-summary.html", [ [ "Description", "org/nnsoft/guice/sli4j/log4j/package-descr.html", null, "" ], [ "Classes", null, [ [ "Log4JLoggerInjector", "org/nnsoft/guice/sli4j/log4j/Log4JLoggerInjector.html", null, "" ], [ "Log4jLoggingModule", "org/nnsoft/guice/sli4j/log4j/Log4jLoggingModule.html", null, "" ] ]
+[ [ "org.nnsoft.guice.sli4j.slf4j", "org/nnsoft/guice/sli4j/slf4j/package-summary.html", [ [ "Description", "org/nnsoft/guice/sli4j/slf4j/package-descr.html", null, "" ], [ "Classes", null, [ [ "Slf4jLoggerInjector", "org/nnsoft/guice/sli4j/slf4j/Slf4jLoggerInjector.html", null, "" ], [ "Slf4jLoggingModule", "org/nnsoft/guice/sli4j/slf4j/Slf4jLoggingModule.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
