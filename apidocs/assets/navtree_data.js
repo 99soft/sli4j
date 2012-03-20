164a81ef08564c0c1ee95e20f808069b92b2de71
simonetripodi <simone.tripodi@gmail.com>
2012-03-20 09:04:05 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,8 @@
+var NAVTREE_DATA =
+[ [ "org.nnsoft.guice.sli4j.core", "org/nnsoft/guice/sli4j/core/package-summary.html", [ [ "Description", "org/nnsoft/guice/sli4j/core/package-descr.html", null, "" ], [ "Classes", null, [ [ "AbstractLoggerInjector", "org/nnsoft/guice/sli4j/core/AbstractLoggerInjector.html", null, "" ], [ "AbstractLoggingModule", "org/nnsoft/guice/sli4j/core/AbstractLoggingModule.html", null, "" ] ]
+, "" ], [ "Annotations", null, [ [ "InjectLogger", "org/nnsoft/guice/sli4j/core/InjectLogger.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,7 @@
+var DATA = [
+      { id:0, label:"org.nnsoft.guice.sli4j.core", link:"org/nnsoft/guice/sli4j/core/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.guice.sli4j.core.AbstractLoggerInjector", link:"org/nnsoft/guice/sli4j/core/AbstractLoggerInjector.html", type:"class" },
+      { id:2, label:"org.nnsoft.guice.sli4j.core.AbstractLoggingModule", link:"org/nnsoft/guice/sli4j/core/AbstractLoggingModule.html", type:"class" },
+      { id:3, label:"org.nnsoft.guice.sli4j.core.InjectLogger", link:"org/nnsoft/guice/sli4j/core/InjectLogger.html", type:"class" }
+
+    ];
