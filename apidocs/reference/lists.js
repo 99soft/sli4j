3ec78a1c5b24808280497047808b194b4a6f4dbd
simonetripodi <simone.tripodi@gmail.com>
2012-03-20 09:06:24 +0100

    Wagon: Deploying site to repository

--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -1,7 +1,6 @@
 var DATA = [
-      { id:0, label:"org.nnsoft.guice.sli4j.testfw", link:"org/nnsoft/guice/sli4j/testfw/package-summary.html", type:"package" },
-      { id:1, label:"org.nnsoft.guice.sli4j.testfw.AbstractLoggerInectionTestCase", link:"org/nnsoft/guice/sli4j/testfw/AbstractLoggerInectionTestCase.html", type:"class" },
-      { id:2, label:"org.nnsoft.guice.sli4j.testfw.Service", link:"org/nnsoft/guice/sli4j/testfw/Service.html", type:"class" },
-      { id:3, label:"org.nnsoft.guice.sli4j.testfw.ServiceImpl", link:"org/nnsoft/guice/sli4j/testfw/ServiceImpl.html", type:"class" }
+      { id:0, label:"org.nnsoft.guice.sli4j.juli", link:"org/nnsoft/guice/sli4j/juli/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.guice.sli4j.juli.JuliLoggerInjector", link:"org/nnsoft/guice/sli4j/juli/JuliLoggerInjector.html", type:"class" },
+      { id:2, label:"org.nnsoft.guice.sli4j.juli.JuliLoggingModule", link:"org/nnsoft/guice/sli4j/juli/JuliLoggingModule.html", type:"class" }
 
     ];
