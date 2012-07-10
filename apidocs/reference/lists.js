ac426136e758b1b37aca0d64f9022bd7ee89ebdd
simonetripodi <simone.tripodi@gmail.com>
2012-07-10 14:05:07 +0200

    Wagon: Deploying site to repository

--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -1,7 +1,7 @@
 var DATA = [
-      { id:0, label:"org.nnsoft.guice.sli4j.core", link:"org/nnsoft/guice/sli4j/core/package-summary.html", type:"package" },
-      { id:1, label:"org.nnsoft.guice.sli4j.core.AbstractLoggerInjector", link:"org/nnsoft/guice/sli4j/core/AbstractLoggerInjector.html", type:"class" },
-      { id:2, label:"org.nnsoft.guice.sli4j.core.AbstractLoggingModule", link:"org/nnsoft/guice/sli4j/core/AbstractLoggingModule.html", type:"class" },
-      { id:3, label:"org.nnsoft.guice.sli4j.core.InjectLogger", link:"org/nnsoft/guice/sli4j/core/InjectLogger.html", type:"class" }
+      { id:0, label:"org.nnsoft.guice.sli4j.testfw", link:"org/nnsoft/guice/sli4j/testfw/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.guice.sli4j.testfw.AbstractLoggerInectionTestCase", link:"org/nnsoft/guice/sli4j/testfw/AbstractLoggerInectionTestCase.html", type:"class" },
+      { id:2, label:"org.nnsoft.guice.sli4j.testfw.Service", link:"org/nnsoft/guice/sli4j/testfw/Service.html", type:"class" },
+      { id:3, label:"org.nnsoft.guice.sli4j.testfw.ServiceImpl", link:"org/nnsoft/guice/sli4j/testfw/ServiceImpl.html", type:"class" }
 
     ];
