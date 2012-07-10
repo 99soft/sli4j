5afce12b11f1e2a886bbbb959a43221b7ef92c7f
simonetripodi <simone.tripodi@gmail.com>
2012-07-10 14:10:09 +0200

    Wagon: Deploying site to repository

--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -1,6 +1,6 @@
 var DATA = [
-      { id:0, label:"org.nnsoft.guice.sli4j.log4j", link:"org/nnsoft/guice/sli4j/log4j/package-summary.html", type:"package" },
-      { id:1, label:"org.nnsoft.guice.sli4j.log4j.Log4JLoggerInjector", link:"org/nnsoft/guice/sli4j/log4j/Log4JLoggerInjector.html", type:"class" },
-      { id:2, label:"org.nnsoft.guice.sli4j.log4j.Log4jLoggingModule", link:"org/nnsoft/guice/sli4j/log4j/Log4jLoggingModule.html", type:"class" }
+      { id:0, label:"org.nnsoft.guice.sli4j.slf4j", link:"org/nnsoft/guice/sli4j/slf4j/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.guice.sli4j.slf4j.Slf4jLoggerInjector", link:"org/nnsoft/guice/sli4j/slf4j/Slf4jLoggerInjector.html", type:"class" },
+      { id:2, label:"org.nnsoft.guice.sli4j.slf4j.Slf4jLoggingModule", link:"org/nnsoft/guice/sli4j/slf4j/Slf4jLoggingModule.html", type:"class" }
 
     ];
